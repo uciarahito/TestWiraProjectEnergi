d29f26d6fcc6e5136c9273e7a94a6b901d3362c0
marthin-pakpahan <marthin@kargo.co.id>
2016-10-20 17:00:46 +0700

    share preference

--- app/src/main/java/uci/develops/wiraenergiproject/helper/SharedPreferenceManager.java
+++ app/src/main/java/uci/develops/wiraenergiproject/helper/SharedPreferenceManager.java
@@ -4,4 +4,7 @@ package uci.develops.wiraenergiproject.helper;
  * Created by Kargo on 10/20/2016.
  */
 public class SharedPreferenceManager {
+    /**
+     * Session manager by shared preference
+     */
 }
